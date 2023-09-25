83cac8b285ec73d1b19be3943c3e129beeac8009
Steve Dower <steve.dower@python.org>
2023-09-25 19:58:29 +0100

    Actually 3.11

--- tests/testdllpack/test-dllpack.py
+++ tests/testdllpack/test-dllpack.py
@@ -79,7 +79,7 @@ assert "data.txt" in c
 
 # importlib.resources has no feature detection, so we have to assume that
 # they'll stick to CPython versions.
-if sys.version_info[:2] >= (3, 10):
+if sys.version_info[:2] >= (3, 11):
     assert (i_r.files(TDP) / "data.txt").read_text().startswith("This is data")
     assert (i_r.files(TDP) / "data.txt").read_bytes().startswith(b"This is data")
 
